b8dc88490b9f9b63e7562b636f1a75833b5a6937
j5richard <j5richardAstudent.bridgew.edu>
2019-09-23 15:03:38 -0400

    work on customer class - start bank class

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -4,5 +4,14 @@ import java.util.ArrayList;
 
 public class Bank {
     private ArrayList<BankAccount> accounts;
-    
+    private ArrayList<Customer> clients;
+
+    public Bank(){
+        accounts = new ArrayList<BankAccount>();
+        clients = new ArrayList<Customer>();
+    }
+
+    public void doBanking(){
+        
+    }
 }
