dae80bf37489a207b9683fd811d8a49621e5d200
j5richard <j5richardAstudent.bridgew.edu>
2019-09-23 14:56:26 -0400

    work on customer class - start bank class

---
+++ src/com/company/Customer.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private int customerID;
+    private String name;
+    private ArrayList<BankAccount> accounts;
+
+    Customer(String customerName, int taxID){
+        name = customerName;
+        customerID = taxID;
+        accounts = new ArrayList<BankAccount>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getID(){
+        return customerID;
+    }
+
+    public boolean openAccount(double initialBalance){
+        var newAccount = new BankAccount(initialBalance, 0.02f);
+        var success = accounts.add(newAccount);
+        return success;
+    }
+
+    public BankAccount closeAccount(int accountID){
+        BankAccount accountToRemove = null;
+        for(var account : accounts){
+            if(account.GetaccountID() == accountID) {
+                accountToRemove = account;
+                break;
+            }
+        }
+        accounts.remove(accountToRemove);
+        return accountToRemove;
+    }
+}
